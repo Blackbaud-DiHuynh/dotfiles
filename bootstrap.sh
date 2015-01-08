bb90e6ab68172693d53126460d09ea5322121807
Di Huynh <di.huynh@blackbaud.com>
2015-01-08 09:42:55 -0600

    vim tmux and bash

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+dir=~/workspace/dotfiles # dotfiles directory
+files=".bashrc .vimrc .vim .tmux.conf" # list of files/folders to symlink in homedir
+
+##########
+  
+cd $dir
+for file in $files; do
+   echo "Creating hard link to $file in home directory."
+   sudo ln ~/$file $dir/$file
+done
+    
+source ~/.bashrc
+source ~/.vimrc 
